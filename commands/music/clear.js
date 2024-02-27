a74e4bf5c0016c9d56789a2fd51489f8f3c90b6c
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-02-27 20:25:26 +0900

    Update clear.js

--- commands/music/clear.js
+++ commands/music/clear.js
@@ -7,12 +7,12 @@ module.exports = {
     async execute(client, message) {
         const queue = client.player.getQueue(message.guild.id);
 
-        if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません ❌`);
+        if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません`);
 
-        if (!queue.tracks[0]) return message.channel.send(`${message.author}, この曲の後に再生する音楽はありません ❌`);
+        if (!queue.tracks[0]) return message.channel.send(`${message.author}, この曲の後に再生する音楽はありません`);
 
         await queue.clear();
 
-        message.channel.send(`プレイリストを削除しました 🗑️`);
+        message.channel.send(`プレイリストを削除しました`);
     },
 };
