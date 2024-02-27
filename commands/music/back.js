b8a1d99d47fed4f4d44466aa2da8cca91720ae46
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-02-27 20:25:05 +0900

    Update back.js

--- commands/music/back.js
+++ commands/music/back.js
@@ -7,12 +7,12 @@ module.exports = {
     async execute(client, message) {
         const queue = client.player.getQueue(message.guild.id);
 
-        if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません ❌`);
+        if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません`);
 
-        if (!queue.previousTracks[1]) return message.channel.send(`${message.author}, 以前再生していた音楽はありません ❌`);
+        if (!queue.previousTracks[1]) return message.channel.send(`${message.author}, 以前再生していた音楽はありません`);
 
         await queue.back();
 
-        message.channel.send(`ひとつ前の音楽を再生します... ✅`);
+        message.channel.send(`ひとつ前の音楽を再生します...`);
     },
 };
