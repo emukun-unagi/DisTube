73d87de6d9333d2b781ef7c731015574ebdb2703
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-02-27 20:29:50 +0900

    Update progress.js

--- commands/music/progress.js
+++ commands/music/progress.js
@@ -7,12 +7,12 @@ module.exports = {
     async execute(client, message) {
         const queue = client.player.getQueue(message.guild.id);
 
-        if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません ❌`);
+        if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません`);
 
         const progress = queue.createProgressBar();
         const timestamp = queue.getPlayerTimestamp();
 
-        if (timestamp.progress == 'Infinity') return message.channel.send(`この曲はライブストリーミングです。表示する再生データはありません。 🎧`);
+        if (timestamp.progress == 'Infinity') return message.channel.send(`この曲はライブストリーミングです。表示する再生データはありません`);
 
         message.channel.send(`${progress} (**${timestamp.progress}**%)`);
     },
