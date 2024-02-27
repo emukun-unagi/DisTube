d75f68e1fe40476aafbe4f043f9ee71051883b35
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-02-27 20:53:38 +0900

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,12 +5,12 @@ module.exports = (client, int) => {
 
     switch (int.customId) {
         case 'saveTrack': {
-          if (!queue || !queue.playing) return int.reply({ content: `現在再生している音楽はありません ❌`, ephemeral: true, components: [] });
+          if (!queue || !queue.playing) return int.reply({ content: `現在再生している音楽はありません`, ephemeral: true, components: [] });
 
-            int.member.send(`**Track Saved: \`${queue.current.title}\` | Posted by \`${queue.current.author}\`, Saved Server: \`${int.member.guild.name}\` ✅**`).then(() => {
-                return int.reply({ content: `DMに音楽のタイトルを送りました ✅`, ephemeral: true, components: [] });
+            int.member.send(`**Track Saved: \`${queue.current.title}\` | Posted by \`${queue.current.author}\`, Saved Server: \`${int.member.guild.name}\`**`).then(() => {
+                return int.reply({ content: `DMに音楽のタイトルを送りました`, ephemeral: true, components: [] });
             }).catch(error => {
-                return int.reply({ content: `DMに音楽のタイトルを送れませんでした ❌`, ephemeral: true, components: [] });
+                return int.reply({ content: `DMに音楽のタイトルを送れませんでした`, ephemeral: true, components: [] });
             });
         }
     }
