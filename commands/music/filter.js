95c76aed7eb2e1b59557ef0f460824a184a0cbe3
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-02-27 20:26:07 +0900

    Update filter.js

--- commands/music/filter.js
+++ commands/music/filter.js
@@ -7,11 +7,11 @@ module.exports = {
     async execute(client, message, args) {
         const queue = client.player.getQueue(message.guild.id);
 
-   if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません ❌`);
+   if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません`);
 
         const actualFilter = queue.getFiltersEnabled()[0];
 
-        if (!args[0]) return message.channel.send(`${message.author}, フィルター名入力してください ❌\n\`bassboost, 8D, nightcore\``);
+        if (!args[0]) return message.channel.send(`${message.author}, フィルター名入力してください\n\`bassboost, 8D, nightcore\``);
 
         const filters = [];
         queue.getFiltersEnabled().map(x => filters.push(x));
@@ -19,7 +19,7 @@ module.exports = {
 
         const filter = filters.find((x) => x.toLowerCase() === args[0].toLowerCase());
 
-        if (!filter) return message.channel.send(`${message.author}, フィルター見つかりませんでした ❌\n\`bassboost, 8D, nightcore\``);
+        if (!filter) return message.channel.send(`${message.author}, フィルター見つかりませんでした\n\`bassboost, 8D, nightcore\``);
 
         const filtersUpdated = {};
 
@@ -27,6 +27,6 @@ module.exports = {
 
         await queue.setFilters(filtersUpdated);
 
-        message.channel.send(`Applied: **${filter}**, Filter Status: **${queue.getFiltersEnabled().includes(filter) ? 'Active' : 'Inactive'}** ✅\n **音楽が長い場合、フィルターの適応時間が長くなることがありますので、ご注意ください。**`);
+        message.channel.send(`Applied: **${filter}**, Filter Status: **${queue.getFiltersEnabled().includes(filter) ? 'Active' : 'Inactive'}**\n **音楽が長い場合、フィルターの適応時間が長くなることがありますので、ご注意ください。**`);
     },
 };
