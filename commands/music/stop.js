317f005d38cb0a5e83d304b687d80f1d47ed7d2a
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-02-27 20:38:28 +0900

    Update stop.js

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -7,10 +7,10 @@ module.exports = {
     execute(client, message) {
         const queue = client.player.getQueue(message.guild.id);
 
-        if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません ❌`);
+        if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません`);
 
         queue.destroy();
 
-        message.channel.send(`このサーバーで再生されていた音楽は停止しました ✅`);
+        message.channel.send(`このサーバーで再生されていた音楽は停止しました`);
     },
 };
