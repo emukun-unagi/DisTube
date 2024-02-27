039d01de2651dcfad7b42e79181cf62c17bbceb1
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-02-27 20:30:51 +0900

    Update resume.js

--- commands/music/resume.js
+++ commands/music/resume.js
@@ -7,10 +7,10 @@ module.exports = {
     execute(client, message) {
         const queue = client.player.getQueue(message.guild.id);
 
-        if (!queue) return message.channel.send(`${message.author}, 現在再生中の音楽はありません ❌`);
+        if (!queue) return message.channel.send(`${message.author}, 現在再生中の音楽はありません`);
 
         const success = queue.setPaused(false);
 
-        return message.channel.send(success ? `**${queue.current.title}**, 曲は続けて再生されます。 ✅` : `${message.author}, どこかで間違っています ❌`);
+        return message.channel.send(success ? `**${queue.current.title}**, 曲は続けて再生されます` : `${message.author}, どこかで間違っています`);
     },
 };
