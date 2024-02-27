e504745fc16ce56401b3ecdf6564f52c29d980a9
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-02-27 20:27:52 +0900

    Update loop.js

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -10,20 +10,20 @@ module.exports = {
         const queue = client.player.getQueue(message.guild.id);
 
  
-if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません ❌`);
+if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません`);
 
         if (args.join('').toLowerCase() === 'queue') {
-            if (queue.repeatMode === 1) return message.channel.send(`${message.author}, 既存のループを無効にする必要があります。 **(${client.config.px}loop)** ❌`);
+            if (queue.repeatMode === 1) return message.channel.send(`${message.author}, 既存のループを無効にする必要があります **(${client.config.px}loop)**`);
 
             const success = queue.setRepeatMode(queue.repeatMode === 0 ? QueueRepeatMode.QUEUE : QueueRepeatMode.OFF);
 
-            return message.channel.send(success ? `Loop Mode: **${queue.repeatMode === 0 ? '無効' : '有効'}**,再生中の音楽を繰り返します 🔁` : `${message.author}, どこかで間違っています ❌`);
+            return message.channel.send(success ? `Loop Mode: **${queue.repeatMode === 0 ? '無効' : '有効'}**,再生中の音楽を繰り返します` : `${message.author}, どこかで間違っています`);
         } else {
-            if (queue.repeatMode === 2) return message.channel.send(`${message.author}, 既存のループを無効にする必要があります。 **(${client.config.px}loop queue)** ❌`);
+            if (queue.repeatMode === 2) return message.channel.send(`${message.author}, 既存のループを無効にする必要があります **(${client.config.px}loop queue)**`);
 
             const success = queue.setRepeatMode(queue.repeatMode === 0 ? QueueRepeatMode.TRACK : QueueRepeatMode.OFF);
 
-            return message.channel.send(success ? `Loop Mode: **${queue.repeatMode === 0 ? '無効' : '有効'}** (**${client.config.px}loop queue**で再生リストにあるすべての音楽を繰り返すことができます) 🔂` : `${message.author}, どこかで間違っています ❌`);
+            return message.channel.send(success ? `Loop Mode: **${queue.repeatMode === 0 ? '無効' : '有効'}** (**${client.config.px}loop queue**で再生リストにあるすべての音楽を繰り返すことができます)` : `${message.author}, どこかで間違っています`);
 };
     },
 };
